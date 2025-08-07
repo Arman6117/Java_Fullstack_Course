6c520a0572f57fa895f3eb2299216b08a238d82f
Arman Patel <vanixgaming634@gmail.com>
2025-08-07 11:50:05 +0530

    Multidimensional array 6-8-2025

---
+++ src/Arrays/TwoDimensionalArray/TakeInput.java
@@ -0,0 +1,8 @@
+package Arrays.TwoDimensionalArray;
+
+public class TakeInput {
+    public static void main(String[] args) {
+        int[][] arr = new int[3][3];
+        System.out.println("Row length: " + arr.length);
+    }
+}
